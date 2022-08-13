d7ea8b85ef9774162735a856a97cf1e83c98f4a0
Alberto Castrignanò <82368828+AlbInitPolito@users.noreply.github.com>
2022-08-13 17:20:56 +0200

    renamed in collect_signals added lock functionality added hashmap retrieve renamed functions added tests

--- collect_signals/src/lib.rs
+++ collect_signals/src/lib.rs
@@ -1,43 +1,53 @@
-pub mod collect_signal {
+pub mod collect_signals {
     use std::collections::{HashMap, VecDeque};
+    use std::env::Args;
     use std::hash::Hash;
-    use std::sync::{Arc, Mutex};
+    use std::sync::{Arc, Mutex, MutexGuard};
 
     #[derive(Clone)]
-    pub struct CollectSignal<S, K, V> {
+    pub struct CollectSignals<S, K, V> {
         command_queue: Arc<Mutex<VecDeque<S>>>,
         collection: Arc<Mutex<HashMap<K, V>>>,
     }
 
-    impl<S, K: Clone + Eq + Hash, V:  Clone + Eq + Hash> CollectSignal<S, K, V> {
+    impl<S, K: Clone + Eq + Hash, V:  Clone + Eq + Hash> CollectSignals<S, K, V> {
         pub fn new() -> Self {
-            return CollectSignal {
+            return CollectSignals {
                 command_queue: Arc::new(Mutex::new(VecDeque::<S>::new())),
                 collection: Arc::new(Mutex::new(HashMap::<K, V>::new()))
             }
         }
 
-        pub fn insert_command(&mut self, command: S) {
+        pub fn insert_signal(&mut self, signal: S) {
             let mut l = self.command_queue.lock().unwrap();
-            l.push_back(command);
+            l.push_back(signal);
         }
 
-        pub fn extract_command(&mut self) -> Option<S> {
+        pub fn extract_signal(&mut self) -> Option<S> {
             let mut l = self.command_queue.lock().unwrap();
             return l.pop_front();
         }
 
-        pub fn insert_collection(&mut self, key: K, value: V) {
+        pub fn insert_entry(&mut self, key: K, value: V) {
             let mut l = self.collection.lock().unwrap();
             l.insert(key, value);
         }
 
         pub fn search_entry(&mut self, key: K) -> Option<V> {
-            let l = self.collection.lock().unwrap();
+            let mut l = self.collection.lock().unwrap();
             return match l.get(&key) {
                 None => None,
                 value => Some(value.unwrap().clone())
             }
+        }
+
+        pub fn produce_hashmap(self) -> HashMap<K,V> {
+            let l = self.collection.lock().unwrap();
+            return l.clone()
+        }
+
+        pub fn lock_hashmap(&mut self) -> MutexGuard<HashMap<K,V>> {
+            return self.collection.lock().unwrap()
         }
     }
 }
