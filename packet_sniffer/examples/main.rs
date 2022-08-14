e76dbd0719813d16c647450a25b55e622e5e5b18
gabbiurlaro <iurlaro.gabriele@gmail.com>
2022-08-14 12:26:41 +0200

    Add an example that show how to use the module

--- packet_sniffer/examples/main.rs
+++ packet_sniffer/examples/main.rs
@@ -1,3 +1,27 @@
+use std::sync::mpsc::channel;
+use std::thread;
+use pcap::Capture;
+
 fn main() {
-    packet_sniffer::test_function();
+    println!("Ex - 02 reading from a pcap file");
+    println!("Multithread example");
+
+    let (tx, rx) = channel();
+    let t1 = thread::spawn(move || {
+        let tx = tx.clone();
+        let mut cap = Capture::from_file("sample_capture.pcap").unwrap();
+        while let Ok(packet) = cap.next() {
+            tx.send(Vec::from(packet.data)).unwrap();
+            //println!("{:?}", Vec::from(packet.data));
+        }
+
+        drop(tx);
+    });
+
+    while let Ok(packet) = rx.recv() {
+        match packet_sniffer::decode_packet(packet) {
+            Ok(()) => (),
+            Err(e) => println!("{:?}", e)
+        }
+    }
 }
