9bd0f7ed7d8a453754a7b052d98d744953ae0ac5
gabbiurlaro <iurlaro.gabriele@gmail.com>
2022-08-28 17:12:50 +0200

    corrected test

--- packet_sniffer/tests/sniffer.rs
+++ packet_sniffer/tests/sniffer.rs
@@ -11,6 +11,8 @@ fn run_without_device_or_file_should_fail() {
     let mut sniffer = Sniffer::new();
     let res = sniffer.run();
     assert!(res.is_err());
+    assert_eq!(sniffer.get_status(), RunStatus::Stop);
     assert_eq!(res.unwrap_err(), SnifferError::UserError("File is null ...".to_string()))
+
 }
 
